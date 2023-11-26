399e83315be0aebccbcef842192b8b2584383526
naoki0810 <naoki.komiya.98ss@gamil.com>
2023-11-26 13:05:08 +0900

    kadai_007提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP_kadai007</title>
+</head>
+<body>
+  <p>
+    <?php
+    $food_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+    print_r($food_data);
+    ?>
+  </p>
+</body>
+</html>
